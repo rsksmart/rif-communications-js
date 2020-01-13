fb6dbdf1ca168a8faf2da3306647a4c2f910a709
Leandro Yalet <leandro@iovlabs.org>
2020-01-13 19:47:00 -0300

    chore: add jsdocs and update tasegir

--- src/modules/peers.ts
+++ src/modules/peers.ts
@@ -1,6 +1,13 @@
 import { create, createFromJSON, createFromPubKey, PeerId } from 'peer-id'
 import { create as peerInfoCreate, PeerInfo } from 'peer-info'
 
+/**
+ * Creates a new PeerInfo instance from an existing PeerId.
+ *
+ * @param pId a PeerId
+ *
+ * @returns Promise<PeerInfo>
+ */
 export function createPeerInfo (pId: PeerId): Promise<PeerInfo> {
   return new Promise<PeerInfo>((resolve, reject) => {
     peerInfoCreate(pId, (err: Error, peer: PeerInfo) => {
@@ -13,6 +20,16 @@ export function createPeerInfo (pId: PeerId): Promise<PeerInfo> {
   })
 }
 
+/**
+ * Creates a PeerId from a JSON object.
+ *
+ * @param {object} keystoreJson - The JSON object that represents a keystore
+ * @param {string} keystoreJson.id - The multihash encoded in base58
+ * @param {string} keystoreJson.pubKey - The public key in protobuf format, encoded in base64
+ * @param {string} keystoreJson.privKey - The private key in protobuf format, encoded in base64
+ *
+ * @returns Promise<PeerId>
+ */
 export function createPeerIdFromJSON (keystoreJson: any): Promise<PeerId> {
   return new Promise<PeerId>((resolve, reject) => {
     createFromJSON(keystoreJson, (err: Error, peerId: PeerId) => {
@@ -25,6 +42,13 @@ export function createPeerIdFromJSON (keystoreJson: any): Promise<PeerId> {
   })
 }
 
+/**
+ * Creates a Peer ID from a buffer containing a public key.
+ *
+ * @param publicKey public key or Buffer
+ *
+ * @returns Promise<PeerId>
+ */
 export function createPeerIdFromPublicKey (publicKey: string | Buffer): Promise<PeerId> {
   return new Promise<PeerId>((resolve, reject) => {
     createFromPubKey(publicKey, (err: Error, peerId: PeerId) => {
@@ -37,6 +61,11 @@ export function createPeerIdFromPublicKey (publicKey: string | Buffer): Promise<
   })
 }
 
+/**
+ * Creates a new Peer ID, complete with public/private keypair.
+ *
+ * @returns Promise<PeerId>
+ */
 export function createKey (): Promise<PeerId> {
   const opts = {
     bits: 256,
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,7 @@
 
+/**
+ * Enum of supported connection modes.
+ */
 export enum ConnectionMode {
     Socket = 'Socket',
     WebRTC = 'WebRTC',
