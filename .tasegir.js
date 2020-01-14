7e638e7eced31db14d3cec14da810e4f20e9448b
Leandro Yalet <leandro@iovlabs.org>
2020-01-14 17:50:05 -0300

    chore: removed js-cids workaround because is fixed on 7.0.2
    Refs: #1, multiformats/js-cid#96

--- .tasegir.js
+++ .tasegir.js
@@ -6,16 +6,6 @@ module.exports = {
             declaration: true
         }
     },
-    // https://github.com/multiformats/js-cid/issues/96
-    webpack: {
-        resolve: {
-            alias: {
-                'multicodec/src/base-table': path.dirname(
-                    require.resolve('multicodec/src/base-table.json')
-                )
-            }
-        },
-    },
     depCheck: {
         ignore: ['mocha', '@types/*', 'tasegir']
     }
